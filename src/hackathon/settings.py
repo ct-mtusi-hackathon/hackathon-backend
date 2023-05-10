03b1d5161bc44001b25817339013bc160d1899e9
Roman Lee (kittzy) <105285331+RG1ee@users.noreply.github.com>
2023-05-10 15:50:29 +0300

    create web.Dockerfile, add postgresql, add drf spectacular (#3)
    * create web.Dockerfile, add postgresql, add drf spectacular
    
    * settings spectacular, add camelCase

--- src/hackathon/settings.py
+++ src/hackathon/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY", default="a;ldkjfa;lkdsjoiwe-342-3")
-DEBUG = bool(os.getenv("DEBUG", default=1))
+DEBUG = bool(os.getenv("DEBUG", 1))
 ALLOWED_HOSTS = ["*"]
 
 
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     # third apps
     "rest_framework",
     "phonenumber_field",
+    "drf_spectacular",
     # my apps
     "src.apps.users",
 ]
@@ -64,8 +65,12 @@ WSGI_APPLICATION = "src.hackathon.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "postgres"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.getenv("POSTGRES_HOST", "127.0.0.1"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
@@ -112,3 +117,28 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_RENDERER_CLASSES": (
+        "djangorestframework_camel_case.render.CamelCaseJSONRenderer",
+    ),
+    "DEFAULT_PARSER_CLASSES": (
+        "djangorestframework_camel_case.parser.CamelCaseJSONParser",
+    ),
+    "JSON_UNDERSCOREIZE": {
+        "no_underscore_before_number": True,
+    },
+}
+
+# SPECTACULAR SETTINGS
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Hackathon Application Server",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "COMPONENT_NO_READ_ONLY_REQUIRED": True,
+    "POSTPROCESSING_HOOKS": [
+        "drf_spectacular.hooks.postprocess_schema_enums",
+        "drf_spectacular.contrib.djangorestframework_camel_case.camelize_serializer_fields",
+    ],
+}
--- src/hackathon/urls.py
+++ src/hackathon/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from src.apps.users.api.views import UserProfileViewSet
 
@@ -8,4 +9,11 @@ from src.apps.users.api.views import UserProfileViewSet
 router = SimpleRouter()
 router.register("users", UserProfileViewSet)
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/v1/", include(router.urls))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/v1/", include(router.urls)),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/v1/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"
+    ),
+]
