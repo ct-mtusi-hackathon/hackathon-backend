9e3e0c736dfc913b9fccd082463b496999bc37a1
RG1ee <niwows@yandex.ru>
2023-05-15 01:22:56 +0300

    create serializers and action for change profile

--- src/apps/users/api/views.py
+++ src/apps/users/api/views.py
@@ -1,9 +1,34 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from src.apps.users.models import User
-from src.apps.users.api.serializers import UserProfileSerializer
+from src.apps.base.api.mixins import PermissionPerAction, SerializerPerAction
+from src.apps.users.api.serializers import (
+    UserProfileSerializer,
+    UserSerializer,
+    UserUpdateProfile,
+)
 
 
-class UserProfileViewSet(viewsets.ModelViewSet):
+class UserProfileViewSet(SerializerPerAction, viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserProfileSerializer
+    action_serializers = {
+        "default": UserSerializer,
+        "profile": UserProfileSerializer,
+        "update_profile": UserUpdateProfile,
+    }
+
+    @action(methods=["GET"], detail=True)
+    def profile(self, request: Request, *args, **kwargs):
+        queryset = self.get_object()
+        serializer = self.get_serializer(queryset)
+        return Response(serializer.data)
+
+    @action(methods=["PATCH"], detail=True)
+    def update_profile(self, request: Request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, instance=request.user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response("Profile changed")
