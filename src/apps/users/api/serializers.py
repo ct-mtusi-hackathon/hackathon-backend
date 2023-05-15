5f4c2508ae68729254a44acb289c8e2512684b1b
Roman Lee (kittzy) <105285331+RG1ee@users.noreply.github.com>
2023-05-15 12:38:19 +0300

    create serializers and action for change profile (#9)
    * create serializers and action for change profile
    
    * refactoring
    
    * configuration ruff linter

--- src/apps/users/api/serializers.py
+++ src/apps/users/api/serializers.py
@@ -1,9 +1,13 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from src.apps.users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    group = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = User
         fields = (
@@ -12,6 +16,7 @@ class UserSerializer(serializers.ModelSerializer):
             "first_name",
             "last_name",
             "patronymic",
+            "group",
         )
 
 
@@ -30,3 +35,56 @@ class UserProfileSerializer(serializers.ModelSerializer):
             "phone_number",
             "group",
         )
+
+
+class UserUpdateProfile(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=(UniqueValidator(queryset=User.objects.all()),),
+    )
+    new_password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
+    confirm_new_password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            "email",
+            "phone_number",
+            "username",
+            "new_password",
+            "confirm_new_password",
+            "telegram_username",
+        )
+
+    def validate_old_password(self, old_password):
+        user = self.context["request"].user
+        if not user.check_password(old_password):
+            raise serializers.ValidationError(
+                {"password": "The old password is incorrect"},
+            )
+
+    def validate_new_password(self, new_password):
+        old_password = self.initial_data.get("old_password")
+        confirm_new_password = self.initial_data.get("confirm_new_password")
+        if old_password == new_password:
+            raise serializers.ValidationError(
+                {"password": "Passwords match"},
+            )
+
+        if new_password != confirm_new_password:
+            raise serializers.ValidationError(
+                {"password": "Password do not match"},
+            )
+
+        return new_password
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data["email"]
+        instance.phone_number = validated_data["phone_number"]
+        instance.username = validated_data["username"]
+        instance.telegram_username = validated_data["telegram_username"]
+        instance.set_password(validated_data["new_password"])
+        instance.save()
+        return instance
