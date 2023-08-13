8f0e1924830abe9250a71d6276eadc4f6774f270
Roman Lee (kittzy) <105285331+RG1ee@users.noreply.github.com>
2023-08-13 10:51:45 +0300

    create new Dockerfile, add redis and celery worker to docker-compose (#21)

--- src/hackathon/settings.py
+++ src/hackathon/settings.py
@@ -174,3 +174,13 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
+
+# CELERY
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", default="redis://127.0.0.1:6379/0")
+CELERY_RESULT_BACKEND = os.getenv(
+    "CELERY_RESULT_BACKEND", default="redis://127.0.0.1:6379/0"
+)
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TIMEZONE = TIME_ZONE
